f9c2772f5d794f4ce661a96ef797d1d0d58c1254
Liam <liam.winterton.dev@gmail.com>
2022-06-11 20:40:18 +0100

    use a basic GraphQL query to get some data

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,24 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 
 import { container,
  heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+	const data = useStaticQuery(graphql`
+		query MyQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}	  
+	`)
+
 	return (
 		<div className={container}>
-			<title>{pageTitle}</title>
+			<title>{pageTitle} | {data.site.siteMetadata.title}</title>
+			<header>{data.site.siteMetadata.title}</header>
 			<nav>
 				<ul className={navLinks}>
 					<li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
