3032f0084bfb486b891ca5383a1bcea9fd599dba
Liam <liam.winterton.dev@gmail.com>
2022-06-12 20:17:24 +0100

    Create a template for MDX blog posts

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost = () => {
+	return (
+		<Layout pageTitle="Super Cool Blog Post!">
+			<p>My blog post content will go here!</p>
+		</Layout>
+	)
+}
+
+export default BlogPost
