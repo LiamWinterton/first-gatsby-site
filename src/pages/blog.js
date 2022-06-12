caa9a0e95ed3a5fb2afbb30cd7331a38a79670dc
Liam <liam.winterton.dev@gmail.com>
2022-06-12 18:52:41 +0100

    This is some weird ass way of getting data into props

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,28 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
 	return (
 		<Layout pageTitle="My Blog Posts">
-			<p>My cool posts will go in here</p>
+			{data.allFile.nodes.map(node => {
+				return (
+					<li key={node.name}>{node.name}</li>
+				)
+			})}
 		</Layout>
 	)
 }
+
+export const query = graphql`
+	query {
+		allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+			nodes {
+				name
+			}
+		}
+	}
+`
 
 export default BlogPage
